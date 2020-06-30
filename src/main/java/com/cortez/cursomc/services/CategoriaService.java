fb84af4c56837834c190c740a917b7899c734957
HerlonCortez <noklyn@gmail.com>
2020-06-30 16:32:38 -0300

    Criando Repository e Service para Categoria

---
+++ src/main/java/com/cortez/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.cortez.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cortez.cursomc.domain.Categoria;
+import com.cortez.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
