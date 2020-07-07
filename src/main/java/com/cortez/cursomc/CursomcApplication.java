4654e541d802f444022ac551b6b500b9c46a4009
HerlonCortez <noklyn@gmail.com>
2020-07-06 22:51:04 -0300

    Pedido, EstadoPagamento e Pagamanto

--- src/main/java/com/cortez/cursomc/CursomcApplication.java
+++ src/main/java/com/cortez/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.cortez.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -13,13 +14,20 @@ import com.cortez.cursomc.domain.Cidade;
 import com.cortez.cursomc.domain.Cliente;
 import com.cortez.cursomc.domain.Endereco;
 import com.cortez.cursomc.domain.Estado;
+import com.cortez.cursomc.domain.Pagamento;
+import com.cortez.cursomc.domain.PagamentoComBoleto;
+import com.cortez.cursomc.domain.PagamentoComCartao;
+import com.cortez.cursomc.domain.Pedido;
 import com.cortez.cursomc.domain.Produto;
+import com.cortez.cursomc.domain.enuns.EstadoPagamento;
 import com.cortez.cursomc.domain.enuns.TipoCliente;
 import com.cortez.cursomc.repositories.CategoriaRepository;
 import com.cortez.cursomc.repositories.CidadeRepository;
 import com.cortez.cursomc.repositories.ClienteRepository;
 import com.cortez.cursomc.repositories.EnderecoRepository;
 import com.cortez.cursomc.repositories.EstadoRepository;
+import com.cortez.cursomc.repositories.PagamentoRepository;
+import com.cortez.cursomc.repositories.PedidoRepository;
 import com.cortez.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -37,7 +45,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
-	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -74,18 +85,34 @@ public class CursomcApplication implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
-		Cliente cli1 = new Cliente(null,"Maria Silva","Maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
-		
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "Maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardin", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "387012", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
 	}
 
 }
